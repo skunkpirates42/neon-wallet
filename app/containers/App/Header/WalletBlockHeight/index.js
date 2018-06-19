71cc09d57b25264b8331656277b8bd4bd92e0780
Maxwell Lasky <mlasky46@gmail.com>
2018-06-18 19:41:39 -0600

    removes duplicate call to get_all_nodes

--- app/containers/App/Header/WalletBlockHeight/index.js
+++ app/containers/App/Header/WalletBlockHeight/index.js
@@ -1,12 +1,14 @@
 // @flow
 import { compose } from 'recompose'
-import { withCall, withData, withRecall } from 'spunky'
+import { withData, withRecall } from 'spunky'
 
 import WalletBlockHeight from './WalletBlockHeight'
 import withNetworkData from '../../../../hocs/withNetworkData'
 import blockHeightActions from '../../../../actions/blockHeightActions'
 
-const mapBlockHeightDataToProps = (blockHeight: ?number): {
+const mapBlockHeightDataToProps = (
+  blockHeight: ?number
+): {
   blockHeight: ?number
 } => ({
   blockHeight
@@ -14,7 +16,6 @@ const mapBlockHeightDataToProps = (blockHeight: ?number): {
 
 export default compose(
   withNetworkData(),
-  withCall(blockHeightActions),
   withData(blockHeightActions, mapBlockHeightDataToProps),
   withRecall(blockHeightActions, ['networkId'])
 )(WalletBlockHeight)
